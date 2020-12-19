0458a62a0dbecca244fce96710420a88c7fe4d8c
ankitdixit9502 <ankitdixit9502>
2020-12-19 15:33:12 +0530

    commit

--- email.php
+++ email.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * Php version 7.2.10
- * 
- * @category Components
- * @package  Packagename
- * @author   Sumit kumar Pandey <pandeysumit399@gmail.com>
- * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
- * @link     http://localhost/training/php%20mysql%20task1/register/signup.php
- */
 session_start();
 $mail=$_SESSION['verify']['mail'];
 $key=$_SESSION['verify']['emailkey'];
